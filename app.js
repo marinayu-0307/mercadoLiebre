50d64e4363a3d6e28200f3f01e058a7ad7d5346d
Marina Yu <marina@Marinas-MacBook-Pro.local>
2021-05-25 01:26:39 -0300

    M04C06 Formularios Register y Login

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const app = express();
 app.listen (3000,() => console.log("Server on"));
 
 app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")))
+app.get("/register",(req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")))
+app.get("/login",(req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")))
+
 
 app.use (express.static(path.resolve(__dirname, './public')));
