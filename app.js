5afd4b42b6711f065c44fc44d24726367f3545b5
Marina Yu <marina@Marinas-MacBook-Pro.local>
2021-05-31 01:58:03 -0300

    Agregado de página de error para los links inexistentes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require("express");
 const path = require("path");
-
+const fs = require("fs");
+const express = require("express");
 const app = express();
 
-app.listen (3000,() => console.log("Server on"));
+app.set("port",process.env.PORT || 3000)
+app.listen(app.get("port"),() => console.log("Server on"));
 
 app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")))
 app.get("/register",(req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")))
 app.get("/login",(req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")))
-app.get("/prueba",(req,res) => res.sendFile(path.resolve(__dirname,"./views/prueba.html")))
+app.get("/error",(req,res) => res.sendFile(path.resolve(__dirname,"./views/error.html")))
 
 
 app.use (express.static(path.resolve(__dirname, './public')));
