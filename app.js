8f555312f7befc77f5034da215a4200d02fc852f
Marina Yu <marina@Marinas-MacBook-Pro.local>
2021-05-30 22:42:51 -0300

    Ajustes header fijo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.listen (3000,() => console.log("Server on"));
 app.get("/",(req,res) => res.sendFile(path.resolve(__dirname,"./views/home.html")))
 app.get("/register",(req,res) => res.sendFile(path.resolve(__dirname,"./views/register.html")))
 app.get("/login",(req,res) => res.sendFile(path.resolve(__dirname,"./views/login.html")))
+app.get("/prueba",(req,res) => res.sendFile(path.resolve(__dirname,"./views/prueba.html")))
 
 
 app.use (express.static(path.resolve(__dirname, './public')));
